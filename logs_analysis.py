ac106aaad37ad090025aec6fdd52da9cd9cb030f
mattybojo <mattbojorquez@gmail.com>
2018-02-17 00:56:29 -0800

    pycodestyle updates

--- logs_analysis.py
+++ logs_analysis.py
@@ -4,7 +4,7 @@ import psycopg2
 DBNAME = 'news'
 question = []
 query = []
-type = ['views', 'views', '%'] # Used for printing result statement
+type = ['views', 'views', '%']  # Used for printing result statement
 
 # Append questions and queries to respective arrays
 
@@ -27,7 +27,7 @@ question.append('Who are the most popular article authors of all time?\n')
 query.append('''
 SELECT au.name, COUNT(*) AS views
 FROM authors au, articles ar, log l
-WHERE au.id = ar.author 
+WHERE au.id = ar.author
 AND l.path = CONCAT('/article/',ar.slug)
 AND l.status LIKE '%200%'
 GROUP BY au.name
@@ -42,13 +42,13 @@ SELECT day, percent_error
 FROM (
     SELECT error.day, ROUND((error_count/total_count)*100,1) AS percent_error
     FROM
-        (SELECT date(time) AS day, 
+        (SELECT date(time) AS day,
             CAST(COUNT(status) AS decimal) AS error_count
         FROM log
         WHERE status LIKE '%404%'
         GROUP BY day) AS error,
 
-        (SELECT DATE(time) AS day, 
+        (SELECT DATE(time) AS day,
             CAST(COUNT(status) AS decimal) AS total_count
         FROM log
         GROUP BY day) AS total
@@ -57,6 +57,7 @@ FROM (
     ) AS percent
 WHERE percent_error > 1.0
 ''')
+
 
 def run_query(query):
     """ Execute the query and return the result set """
@@ -67,6 +68,7 @@ def run_query(query):
     db.close()
     return result
 
+
 def print_query_result(question, query, type):
     """ Pretty print the results of the executed query """
     print(question)
@@ -74,6 +76,7 @@ def print_query_result(question, query, type):
     for i in range(len(res)):
         print("\t{0} -- {1} {2}".format(res[i][0], res[i][1], type))
     print('\n')
+
 
 # Loop through every query and print the results
 for i in range(len(query)):
